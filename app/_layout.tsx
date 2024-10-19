3ec4774b701a02ae29eeb205e233d34573099469
Nikhil Acharya <nohaxjustchickn@gmail.com>
2024-10-19 10:51:37 -0400

    started layout

---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../global.css";
+import "expo-dev-client";
+import { Stack } from "expo-router";
+import ErrorBoundary from "react-native-error-boundary";
+
+export default function AppLayout() {
+  return (
+    <>
+      <ErrorBoundary>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </ErrorBoundary>
+    </>
+  );
+}
